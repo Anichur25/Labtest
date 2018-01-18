28fec51c1f046c9038f94c9fde5b275b6e17fcaa
anis25 <aniscseru6125@gmail.com>
2018-01-19 01:39:52 +0600

    This is one type of program which determine the hottest temperature....

---
+++ src/Labtest/HottestTemperature.java
@@ -0,0 +1,62 @@
+package Labtest;
+
+import edu.duke.*;
+import org.apache.commons.csv.*;
+import java.io.*;
+
+public class HottestTemperature {
+
+    public static void main(String[] args) {
+        HottestTemperature ob = new HottestTemperature();
+        ob.testHottestTemperatureInDay();
+        ob.testHottestTemperatureInManyDays();
+    }
+    private void testHottestTemperatureInManyDays() {
+        CSVRecord largest = hottestTemperatureInManyDays();
+        System.out.println("Hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("DateUTC"));
+
+    }
+    private CSVRecord hottestTemperatureInManyDays() {
+        CSVRecord largestSoFar = null;
+        DirectoryResource dr = new DirectoryResource();
+        for(File f : dr.selectedFiles()){
+            FileResource fr = new FileResource(f);
+            CSVRecord currentRow = hottestHourInFile(fr.getCSVParser());
+            if(largestSoFar == null){
+                largestSoFar = currentRow;
+            }
+            else{
+                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+                double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+                if(currentTemp > largestTemp){
+                    largestSoFar = currentRow;
+                }
+            }
+        }
+        return largestSoFar;
+    }
+    private void testHottestTemperatureInDay() {
+        FileResource fr = new FileResource();
+        CSVRecord largest = hottestHourInFile(fr.getCSVParser());
+        System.out.println("Hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("TimeEST"));
+    }
+    private CSVRecord hottestHourInFile(CSVParser csvParser) {
+        CSVRecord largestSoFar = null;
+        for(CSVRecord currentRow : csvParser){
+            if(largestSoFar == null){
+                largestSoFar = currentRow;
+            }
+            else{
+                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+                double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+                if(currentTemp > largestTemp){
+                    largestSoFar = currentRow;
+                }
+
+            }
+        }
+        return largestSoFar;
+    }
+
+
+}
